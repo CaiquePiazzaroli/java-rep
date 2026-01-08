b8f88f6b48acefd683ad6b7ffde7e8328f1da177
caique <caique405@gmail.com>
2026-01-08 20:58:48 -0300

    tada types, Operators

---
+++ Aula14PalavraChaveVar.java
@@ -0,0 +1,11 @@
+public class Aula14PalavraChaveVar {
+    public static void main(String[] args) {
+
+        var name = "Caique Mendes";
+        System.out.println(name);
+
+        var altura = 1.97f;
+        System.out.println(altura);
+
+    }
+}
