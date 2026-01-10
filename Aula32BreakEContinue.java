45cc0fcf549bdb48f654f84fddd672f05af30d1c
caique <caique405@gmail.com>
2026-01-10 13:31:24 -0300

    Loops e arrays

---
+++ Aula32BreakEContinue.java
@@ -0,0 +1,21 @@
+public class Aula32BreakEContinue {
+    public static void main(String[] args) {
+        // Usando o break
+        for(int i = 1; i < 100; i++) {
+            System.out.println(i);
+            if(i == 75) {
+                System.out.println("O valor de i é " + i + " parando o loop");
+                break;
+            }
+        }
+
+        // Usando o continue
+        for(int i = 1; i < 100; i++) {
+            if(i % 2 == 0) {
+                System.out.println("O valor de i é " + i + " pulando o loop");
+                continue;
+            }
+            System.out.println("loop rodou pela " + i + "°" + " vez");
+        }
+    }
+}
