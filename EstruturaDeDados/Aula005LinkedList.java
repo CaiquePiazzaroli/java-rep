6947bdee633c8182176faf2f96f8987b9c9e3a8a
caique <caique405@gmail.com>
2026-03-01 18:11:19 -0300

    LinkedList

---
+++ EstruturaDeDados/Aula005LinkedList.java
@@ -0,0 +1,20 @@
+package EstruturaDeDados;
+import java.util.List;
+import java.util.LinkedList;
+
+public class Aula005LinkedList {
+    public static void main(String[] args) {
+        List<String> frutas = new LinkedList<String>();
+
+        frutas.add("Banana");
+        frutas.add("Maçã");
+        frutas.add("Pera");
+        frutas.add("Limão");
+
+        // Acessando o primeiro elemento
+        System.out.println(frutas.get(0));
+
+        // Tamanho da lista
+        System.out.println(frutas.size());
+    }
+}
