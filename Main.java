34107c4774ad3cba4800d4cd9c64f4d19fbe286c
caique <caique405@gmail.com>
2026-05-03 11:44:11 -0300

    rev: Wrapper

--- Main.java
+++ Main.java
@@ -2,30 +2,20 @@
 
 public class Main {
     public static void main(String[] args) {
-        // Maior entre dois numeros
-        int maior = Math.max(40, 30);
-        System.out.println(maior); // 40
         
-        int menor = Math.min(55, 97);
-        System.out.println(menor); // 55
 
-        double raiz = Math.sqrt(9);
-        System.out.println(raiz); // 3.0
+        //Value of
+        Integer numero1 = Integer.valueOf(20); // primitivo
+        System.out.println(numero1); //OK
 
-        int absoluto = Math.abs(-3);
-        System.out.println(absoluto); // 3
+        Integer numero2 = Integer.valueOf("45"); // string
+        System.out.println(numero2); //OK
 
-        int pow = Math.powExact(2, 3);
-        System.out.println(pow); // 8
+        // Integer numero3 = Integer.valueOf(" 45 "); // Exeption: java.lang.NumberFormatException
+        // System.out.println(numero3); // NOTOK
 
-        double arrCima = Math.ceil(5.5);
-        System.out.println(arrCima); // 6.0
-
-        double arrBaixo = Math.floor(4.3);
-        System.out.println(arrBaixo); // 4.0
-
-        double aleatorio = Math.random();
-        System.out.println(aleatorio); // Um numero entre 0.0 e 1.0
+        Integer numero4 = Integer.valueOf("45a"); // Exeption: java.lang.NumberFormatException
+        System.out.println(numero4); // NOTOK
 
     }
 }
