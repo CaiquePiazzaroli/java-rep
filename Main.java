856dd8a1e4525456f401a12f825d581244dce67b
caique <caique405@gmail.com>
2026-05-09 19:47:56 -0300

    rev: Metodos de criação classe Files

--- Main.java
+++ Main.java
@@ -1,3 +1,5 @@
+import java.io.File;
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -5,42 +7,32 @@ import java.nio.file.Paths;
 public class Main {
     public static void main(String[] args) {
         
-        // Diretórios
-        Path diretorioInexistente = Paths.get("C:", "arquivosInexistente");
-        Path diretorio = Paths.get("C:", "arquivos");
-
-        // Arquivos
-        Path arquivoIntexistente = Paths.get("C:", "arquivos", "arquivoInexistente.txt");
-        Path arquivo= Paths.get("C:", "arquivos", "caique_arquivo.txt"); 
-
-        // Diretório existente
-        System.out.println("Diretorio existente");
-        System.out.println(Files.exists(diretorio)); // true
-        System.out.println(Files.notExists(diretorio)); // false
-        System.out.println(Files.isDirectory(diretorio)); // true
-        System.out.println(Files.isRegularFile(diretorio)); // false
-
-        // Diretório Inexistente
-        System.out.println("Diretório Inexistente");
-        System.out.println(Files.exists(diretorioInexistente)); // false
-        System.out.println(Files.notExists(diretorioInexistente)); // true
-        System.out.println(Files.isDirectory(diretorioInexistente)); // false
-        System.out.println(Files.isRegularFile(diretorioInexistente)); // false
+        Path diretorio = Paths.get("c:", "revisao", "criacaoDeArquivos");
 
 
-        // Arquivo existente
-        System.out.println("Arquivo existente");
-        System.out.println(Files.exists(arquivo)); // true
-        System.out.println(Files.notExists(arquivo)); // false
-        System.out.println(Files.isDirectory(arquivo)); // false
-        System.out.println(Files.isRegularFile(arquivo)); // true
+        //System.out.println(System.getProperty("java.io.tmpdir"));
 
-        // Arquivo Inexistente
-        System.out.println("Arquivo Inexistente");
-        System.out.println(Files.exists(arquivoIntexistente)); // false
-        System.out.println(Files.notExists(arquivoIntexistente)); // true
-        System.out.println(Files.isDirectory(arquivoIntexistente)); // false
-        System.out.println(Files.isRegularFile(arquivoIntexistente)); // false
+        try {
+            // CreateDirectory
+            // Cria um direório dentro de um sistema de arquivos 
+            // Ex -> a pasta revisao ja existe, createDirectory ira criar apenas a pasta criaçãoDeArquivos
+            if(!Files.exists(diretorio)) Files.createDirectory(diretorio); 
+
+            // CreateDirectories
+            // Cria as pastas recursivamente dentro de um sistema de arquivos
+            Path diretorioRecursivo = Paths.get("c:","diretorioRecursivo", "metodosDeCriacao", "criandoDir");
+
+            // Cria C:\diretorioRecursivo\metodosDeCriacao\criandoDir
+            Path caminhoRecursivoCriado = Files.createDirectories(diretorioRecursivo);
+
+            // createFile: Cria arquivos]
+            Path criado = Files.createFile(diretorio.resolve("meuNovoArquivo.txt"));
+            System.out.println(criado);
+
+
+        } catch (IOException e) {
+            System.out.println(e);
+        }
 
     }
 }
