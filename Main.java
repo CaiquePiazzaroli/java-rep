04f88e383ed916891cb4032b2e4699bd5f68a3a3
caique <caique405@gmail.com>
2026-05-12 11:26:04 -0300

    Rev: Leitura e escrita classe files

--- Main.java
+++ Main.java
@@ -1,51 +1,26 @@
-import java.io.File;
-import java.io.FileOutputStream;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.net.URI;
-import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.nio.file.StandardCopyOption;
+import java.nio.file.StandardOpenOption;
 
 public class Main {
     public static void main(String[] args) {
         
-        // Transferindo arquivos da pasta x para pasta y no sistema de arquivos
-        Path arquivoOrigem = Paths.get("C:", "revisao", "pasta1", "arquivo.txt");
-        Path arquivoDestino = Paths.get("C:", "revisao", "pasta2", "arquivo.txt");
-        try {
-            Path resultado = Files.copy(arquivoOrigem, arquivoDestino, StandardCopyOption.REPLACE_EXISTING);
-            System.out.println(resultado);
-        } catch (Exception e) {
-            System.out.println(e);
-        }
-        
+        Path imagem = Paths.get("C:", "revisao", "pasta1", "arquivo.txt");
 
-        // Copiando de um InputStream (na web)
-        Path arquivoDestinoInputStream = Paths.get("C:", "revisao", "pasta2", "imagem.png");
         try {
-            URI uri = new URI("https://cataas.com/cat");
-
-            try(InputStream in =  uri.toURL().openStream()) {
-                Files.copy(in, arquivoDestinoInputStream, StandardCopyOption.REPLACE_EXISTING);
-                System.out.println("Conteúdo salvo em: " + arquivoDestinoInputStream);
+            try (BufferedWriter writer = Files.newBufferedWriter(imagem)) {
+                writer.write("Primeira linha do arquivo.");
+                writer.newLine();
+                writer.write("Segunda linha do arquivo.");
+                writer.newLine();
             }
         } catch (Exception e) {
             System.out.println(e);
-        }
-
-        // Copiando arquivo para outro com outputStream
-        Path arquivoOrigemOutPutStream = Paths.get("C:", "revisao", "pasta2", "imagem.png");
-        try {
-            try(OutputStream out = new FileOutputStream("C:\\revisao\\pasta1\\imagem1.png")) {
-                long bytesCopiados = Files.copy(arquivoOrigemOutPutStream, out);
-                System.out.println(bytesCopiados);
-            }
-        } catch (Exception e) {
-            // TODO: handle exception
-        }
+        }    
     }
+
 }
