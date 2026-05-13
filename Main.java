e6202e5b5be11bfe8cf339aa751fc1b23a116a49
caique <caique405@gmail.com>
2026-05-13 15:42:00 -0300

    rev Files:list

--- Main.java
+++ Main.java
@@ -9,7 +9,7 @@ public class Main {
     public static void main(String[] args) {
         
         // C:\revisao\pasta1
-        Path diretorio = Paths.get("C:","revisao", "pasta1");
+        Path diretorio = Paths.get("C:","revisao");
         if (!Files.exists(diretorio)) {
             try {
                 Files.createDirectories(diretorio);
@@ -19,9 +19,8 @@ public class Main {
         }
 
         try {
-            Path arquivo = diretorio.resolve("EscrevendoComInputStream.txt");
-            try(Stream<String> linhas = Files.lines(arquivo)) {
-                linhas.forEach(System.out::println);
+            try(Stream<Path> caminhos = Files.list(diretorio)) {
+                caminhos.forEach(System.out::println); // pasta1 e pasta2
             }
         } catch (Exception e) {
             System.out.println(e);
