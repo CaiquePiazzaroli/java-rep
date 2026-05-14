dbb79a158b3c754174d69ff2f1201e7d5f612f36
caique <caique405@gmail.com>
2026-05-14 13:02:40 -0300

    Teste arraylist x LinkedList

--- Main.java
+++ Main.java
@@ -1,26 +1,47 @@
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
+        
+        final int MAX = 200000;
 
-        Path arquivo = Paths.get("C:","revisao", "pasta1", "EscrevendoComInputStream.txt");
-
-
-        System.out.println(Files.isReadable(arquivo)); // true
-        System.out.println(Files.isWritable(arquivo)); // true
-        System.out.println(Files.isExecutable(arquivo)); // true
-
-        try {
-            System.out.println(Files.getLastModifiedTime(arquivo)); // 2026-05-12T17:50:00.9607495Z
-            System.out.println(Files.size(arquivo)); // Retorna o tamanho do arquivo em bytes
-        } catch (Exception e) {
-
+        //Teste de velocidade ArrayList
+        
+        
+        List<Integer> listaArrayList = new ArrayList<Integer>();
+        
+        //ArrayList é mais lento para inserção e remoção: 4s
+        for (int i = 0; i < MAX; i++) {
+            listaArrayList.add(i);
         }
+        
+        // ArrayList é mais rapido para acessar elementos: 1s
+        long tInicioArrayList = System.currentTimeMillis();
+        for (int i = 0; i < listaArrayList.size(); i++) {
+            listaArrayList.get(i);
+        }
+        long tFimArrayList = System.currentTimeMillis();
+        
+        System.out.println("Tempo total: " + (tFimArrayList - tInicioArrayList)); // Resultado 74
 
+
+        // Teste de velocidade ArrayList
+        List<Integer> listaLinkedList = new LinkedList<Integer>();
         
+        // LinkedList é mais rapido para inserção e remoção tempo: 3s
+        for (int i = 0; i < MAX; i++) {
+            listaLinkedList.add(i);
+        }
         
+        long tInicioLinkedList = System.currentTimeMillis();
+        for (int i = 0; i < listaLinkedList.size(); i++) {
+            listaLinkedList.get(i);
+        }
+        long tFimLinkedList = System.currentTimeMillis();
+        
+        System.out.println("Tempo total: " + (tFimLinkedList - tInicioLinkedList)); // Resultado 280
 
     }
 }
