aeca642d18fb217ccbbd485aa320c1dda014e4a9
caique <caique405@gmail.com>
2026-05-19 13:18:43 -0300

    Rev: Narrowing casting e Wideningcasting

--- Main.java
+++ Main.java
@@ -1,9 +1,16 @@
 public class Main {
     public static void main(String[] args) {
 
-        int variavelDeclarada; // Declarando uma variavel
-        variavelDeclarada = 40; // Atribuindo uma valor a uma variável
-        variavelDeclarada = 60; // Retribuindo uma valor a uma variável
+        float valor1 = 456.90f; // 32 bits ou 4 Bytes (Menor em espaço)
+        double valor2 = 97.42; // 64 bits ou 8 Bytes (Maior em espaço)
+
+        // Widening (Wide - Largo) cast ou cast implícito: quando converte um tipo menor em um maior
+        double novoValor1 = valor1;
+        System.out.println(novoValor1);
+
+        // Narrowing  (narrow - estreito) cast ou cast explícito: Quando converte-se um tipo maior em um tipo menor
+        float novoValor2 = (float) valor2;
+        System.out.println(novoValor2);
         
     }
 }
