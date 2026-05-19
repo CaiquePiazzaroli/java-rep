a6c7a8fde5a8c74576168d4cb6f699b245faf640
caique <caique405@gmail.com>
2026-05-19 13:04:26 -0300

    Rev: Declaração, Atribuição e reatribuição

--- Main.java
+++ Main.java
@@ -1,13 +1,9 @@
 public class Main {
     public static void main(String[] args) {
 
-        // Declarei e nao atribuí nenhum valor
-        int numero;
-
-        //System.out.println(numero); // Erro: endereço de memória nao foi limpo previamente
-
-        MinhaClasse mc = new MinhaClasse(); 
-        System.out.println(mc.numero); // 0 => Endereço de memória foi limpo previamente e atribuido valores padrao
+        int variavelDeclarada; // Declarando uma variavel
+        variavelDeclarada = 40; // Atribuindo uma valor a uma variável
+        variavelDeclarada = 60; // Retribuindo uma valor a uma variável
         
     }
 }
