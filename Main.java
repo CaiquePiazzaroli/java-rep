7fc619ac6ff214de4b1920e9ab3c9f7f9aa07a4d
caique <caique405@gmail.com>
2026-05-19 12:49:45 -0300

    Rev: Tipos primitivos

--- Main.java
+++ Main.java
@@ -1,22 +1,22 @@
-import java.util.PriorityQueue;
-
 public class Main {
     public static void main(String[] args) {
 
+        boolean b1 = false; // 1 bit
+        
+        byte n1 = 127; // 8 bits ou 1 Byte
+        byte n2 = -128; 
+        
+        short n3 = -32768; // 16 bits ou 2 Bytes
+        short n4 = 32767;
+        char c1 = 'b'; // 16 bits => aspas simples
 
-        // Ordanando uma Fila por meio de um enum
-        PriorityQueue <Aluno> alunos = new PriorityQueue<>();
-        alunos.add(new Aluno("Felipe Alves", 10.0 , MeuEnum.ABRIL));
-        alunos.add(new Aluno("Marcelo", 10.0 , MeuEnum.JANEIRO));
-        alunos.add(new Aluno("Amanda", 10.0 , MeuEnum.MAIO));
-        alunos.add(new Aluno("Natanael", 10.0 , MeuEnum.FEVEREIRO));
-        alunos.add(new Aluno("Amorim", 10.0 , MeuEnum.ABRIL));
+        int n5 = 80545; // 32 bits ou 4 Bytes -2bi até +2bi
+        float n6 = 7.55687789f; // 32 bits ou 4 Bytes => 6 casas decimais de precisao
+        System.out.println(n6);
 
-        System.out.println(alunos.poll()); // Marcelo
-        System.out.println(alunos.poll()); // Natanael 2
-        System.out.println(alunos.poll()); // Felipe Alves 4
-        System.out.println(alunos.poll()); // Amorim 4
-        System.out.println(alunos.poll()); // Amanda 5
+
+        long n7 = 43901273816L; // 64 bits ou 8 Bytes
+        double n8 = 7.55687789; // 64 bit ou 8 Bytes => Alta precisao de casas decimais
         
     }
 }
