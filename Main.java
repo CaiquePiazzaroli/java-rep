dc76b6c59341d6fc410427a4adc409e16521e977
caique <caique405@gmail.com>
2026-05-21 13:54:22 -0300

    rev: estruturas de decisao

--- Main.java
+++ Main.java
@@ -6,16 +6,99 @@ public class Main {
     // esc. global
     static int meuNumero = 40;
 
-
     public static void main(String[] args) {
-        int[] nums = new int[] {2, 9, 4,10, 90};
-        int target = 13;
-        for(int i = 0; i < nums.length; i++) {
-            for(int j = i+1; j < nums.length; j++) {
-                if(nums[i] + nums[j] == target) {
-                    System.out.println(i + " " + j);
-                }
-            }
+
+        // Estrutura simples de decisao
+        if (true) {
+            System.out.println("é verdade este bilhete");
         }
+
+        // Estrutura simples sem chaves (deve conter apenas 1 linha)
+        if (true)
+            System.out.println("É verdade este bilhete novamente");
+
+        // Estrutura de decisao composta
+        int nota = 8;
+        if (nota >= 7) {
+            System.out.println("Aprovado"); // Excuta se nota > ou = a 7
+        } else {
+            System.out.println("Reprovado"); // Executa se nota for < 7
+        }
+
+        // Estrutura de decisao composta sem chaves (deve conter apenas 1 linha)
+        double precoCasa = 249000.90;
+        if (precoCasa <= 300000)
+            System.out.println("Compra a casa"); // Compra se o preço da casa for < que 300k
+        else
+            System.out.println("Busca empréstimo"); // Busca empréstimo caso a casa seja maior que 300k
+
+        // Estruturas condicionais encadeadas
+        int anoCarro = 2015;
+        if (anoCarro > 2014) {
+            System.out.println("Compra na hora!");
+        } else if (anoCarro > 2010 && anoCarro <= 2014) {
+            System.out.println("Pensa na proposta");
+        } else {
+            System.out.println("Nem considera a compra!");
+        }
+
+        // if ternário ou operador ternário -> Atribui dinamicamente um valor a uma
+        // variavel
+        int nota2 = 6;
+        String resultado = nota2 >= 7 ? "Aprovado" : "Reprovado";
+        System.out.println("Resultado = " + resultado);
+
+        // IF aninhado => realizar um if dentro de outro if
+        int age = 20;
+        boolean isCitizen = true;
+
+        if (age >= 18) {
+            System.out.println("Old enough to vote.");
+            if (isCitizen) {
+                System.out.println("And you are a citizen, so you can vote!");
+            } else {
+                System.out.println("But you must be a citizen to vote.");
+            }
+        } else {
+            System.out.println("Not old enough to vote.");
+        }
+
+        // Switch case
+        int day = 10;
+        switch (day) {
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("Thursday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            case 6:
+                System.out.println("Saturday");
+                break;
+            case 7:
+                System.out.println("Sunday");
+                break;
+            default:
+                System.out.println("Não foi encontrado dias correspondentes!");
+        }
+
+
+        // Retorna um valor
+        String descricaoDia = switch (day) {
+            case 1 -> "Segunda";
+            case 2 -> "Terça";
+            default -> "Dia inválido";
+        };
+
+
     }
 }
