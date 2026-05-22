e330471bf01dad6a5d51d7b980961bf6b37e4152
caique <caique405@gmail.com>
2026-05-22 10:21:04 -0300

    rec: estruturas de repetição

--- EstruturaDeDados/Aula005CPilhas.java
+++ EstruturaDeDados/Aula005CPilhas.java
@@ -6,33 +6,36 @@ import java.util.Queue;
 
 public class Aula005CPilhas {
     public static void main(String[] args) {
+        
         // Criando uma pilha de Strings usando LinkedList (LIFO)
         Deque<String> livros = new LinkedList<>();
 
         // Empilhando (Push)
         livros.push("Senhor dos aneis");
-
         System.out.println("Com 1 elemento: " + livros); 
 
         livros.push("A guerra dos tronos");
-
         System.out.println("Com 2 elemento: " + livros); 
 
         livros.push("Duna");
-
         System.out.println("Com 3 elemento: " + livros); 
-
         livros.push("Hobbit");
+
+        // Imprimindo a pilha
                                                         //  (Topo)                                 (Fundo)
         System.out.println("Pilha inicial: " + livros); // [Hobbit, Duna, A guerra dos tronos, Senhor dos aneis]
 
+        // Imprimindo o primeiro e o ultimo
+        System.out.println("Primeiro: " + livros.getFirst()); // Hobbit
+        System.out.println("Ultimo da fila: " + livros.getLast()); // Senhor dos aneis
+        
         // Espiando o topo (Peek)
         System.out.println("Topo da pilha: " + livros.peek()); // Saída: Hobbit
+        System.out.println("Espiando o ultimo: " + livros.peekLast()); // Senhor dos aneis
 
         // Desempilhando (Pop)
         System.out.println("Removido: " + livros.pop()); // Desempilha Hobbit
         System.out.println("Novo topo: " + livros.peek()); // Duna
-
         System.out.println("Pilha Final: " + livros); // [Duna, A guerra dos tronos, Senhor dos aneis]
     }
 }
--- Main.java
+++ Main.java
@@ -1,104 +1,46 @@
-import java.util.Arrays;
-import java.util.List;
 
 public class Main {
-
-    // esc. global
-    static int meuNumero = 40;
-
     public static void main(String[] args) {
 
-        // Estrutura simples de decisao
-        if (true) {
-            System.out.println("é verdade este bilhete");
+        // while
+        int count = 1;
+        while (count < 10) {
+            System.out.println("While >> " + count);
+            count++;
         }
 
-        // Estrutura simples sem chaves (deve conter apenas 1 linha)
-        if (true)
-            System.out.println("É verdade este bilhete novamente");
+        // dowhile executa pelo menos 1x
+        do {
+            System.out.println("Do while >> " + count);
+            count++;
+        } while (count < 10);
 
-        // Estrutura de decisao composta
-        int nota = 8;
-        if (nota >= 7) {
-            System.out.println("Aprovado"); // Excuta se nota > ou = a 7
-        } else {
-            System.out.println("Reprovado"); // Executa se nota for < 7
+
+        // for
+        for(int i = 0; i < 5; i++) {
+            System.out.println("For: " + i);
+        } 
+
+
+        // for each
+        String[] nomes = new String[] {"Joao", "Maria", "José", "Desconhecido", "Cleide", "Lindomar", "Daniel"};
+        for(String nome : nomes) {
+
+            // Continue Pula para proxmia iteração
+            if(nome.equalsIgnoreCase("desconhecido")) continue;
+
+
+            // Break Encerra
+            if(nome.equalsIgnoreCase("CLEIDE")) {
+                System.out.print("Achamos a Cleide - ");
+                System.out.println("Nome: " + nome);
+                break;
+            };
+
+            
+            System.out.println(nome);
         }
-
-        // Estrutura de decisao composta sem chaves (deve conter apenas 1 linha)
-        double precoCasa = 249000.90;
-        if (precoCasa <= 300000)
-            System.out.println("Compra a casa"); // Compra se o preço da casa for < que 300k
-        else
-            System.out.println("Busca empréstimo"); // Busca empréstimo caso a casa seja maior que 300k
-
-        // Estruturas condicionais encadeadas
-        int anoCarro = 2015;
-        if (anoCarro > 2014) {
-            System.out.println("Compra na hora!");
-        } else if (anoCarro > 2010 && anoCarro <= 2014) {
-            System.out.println("Pensa na proposta");
-        } else {
-            System.out.println("Nem considera a compra!");
-        }
-
-        // if ternário ou operador ternário -> Atribui dinamicamente um valor a uma
-        // variavel
-        int nota2 = 6;
-        String resultado = nota2 >= 7 ? "Aprovado" : "Reprovado";
-        System.out.println("Resultado = " + resultado);
-
-        // IF aninhado => realizar um if dentro de outro if
-        int age = 20;
-        boolean isCitizen = true;
-
-        if (age >= 18) {
-            System.out.println("Old enough to vote.");
-            if (isCitizen) {
-                System.out.println("And you are a citizen, so you can vote!");
-            } else {
-                System.out.println("But you must be a citizen to vote.");
-            }
-        } else {
-            System.out.println("Not old enough to vote.");
-        }
-
-        // Switch case
-        int day = 10;
-        switch (day) {
-            case 1:
-                System.out.println("Monday");
-                break;
-            case 2:
-                System.out.println("Tuesday");
-                break;
-            case 3:
-                System.out.println("Wednesday");
-                break;
-            case 4:
-                System.out.println("Thursday");
-                break;
-            case 5:
-                System.out.println("Friday");
-                break;
-            case 6:
-                System.out.println("Saturday");
-                break;
-            case 7:
-                System.out.println("Sunday");
-                break;
-            default:
-                System.out.println("Não foi encontrado dias correspondentes!");
-        }
-
-
-        // Retorna um valor
-        String descricaoDia = switch (day) {
-            case 1 -> "Segunda";
-            case 2 -> "Terça";
-            default -> "Dia inválido";
-        };
-
+        
 
     }
 }
