9baf092a140c5ca83dd38e801ff96670e14e6c71
caique <caique405@gmail.com>
2026-05-24 12:09:51 -0300

    Rev: Arrays e Matrizes

--- Main.java
+++ Main.java
@@ -1,46 +1,66 @@
-
 public class Main {
     public static void main(String[] args) {
-
-        // while
-        int count = 1;
-        while (count < 10) {
-            System.out.println("While >> " + count);
-            count++;
-        }
-
-        // dowhile executa pelo menos 1x
-        do {
-            System.out.println("Do while >> " + count);
-            count++;
-        } while (count < 10);
-
-
-        // for
-        for(int i = 0; i < 5; i++) {
-            System.out.println("For: " + i);
-        } 
-
-
-        // for each
-        String[] nomes = new String[] {"Joao", "Maria", "José", "Desconhecido", "Cleide", "Lindomar", "Daniel"};
-        for(String nome : nomes) {
-
-            // Continue Pula para proxmia iteração
-            if(nome.equalsIgnoreCase("desconhecido")) continue;
-
-
-            // Break Encerra
-            if(nome.equalsIgnoreCase("CLEIDE")) {
-                System.out.print("Achamos a Cleide - ");
-                System.out.println("Nome: " + nome);
-                break;
-            };
-
-            
-            System.out.println(nome);
-        }
         
+        // Declaração
+        int numeros1[];
+        int[] numeros2; // -> Mais recomendado!
+
+        // Declaração + inicialização
+        int[] numeros3 = new int[5];
+
+        // Lendo array sem atribuição -> Imprime zeros
+        for(int numero : numeros3) {
+            System.out.println(numero); // 0
+        }
+
+        // Atribuindo valores aos elementos do array
+        for(int i = 0; i < numeros3.length; i++) {
+            numeros3[i] = 10; // 10
+        }
+
+        // Re imprimindo o array
+        for(int numero : numeros3) {
+            System.out.println(numero); // 10
+        }
+
+
+        // Inicialização com valores
+        String[] nomes = new String[] {"Caique", "Joao", "Maria"};
+        String[] nomes1 = {"Marcelo", "Elisandra", "Jandira"};
+
+
+        // Acessando elementos
+        System.out.println(nomes1[0]);
+        System.out.println(nomes1[1]);
+        System.out.println(nomes1[2]);
+        try {
+            
+            System.out.println(nomes1[3]); // java.lang.ArrayIndexOutOfBoundsException
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.err.println("Não foi possivel acessar o elemento: " + e);
+        }
+
+
+        // Alterando elemento de um array
+        nomes1[2] = "Julaide";
+        System.out.println(nomes1[2]); // Julaide
+
+        // Matriz
+
+        int[][] myNumbers = { {1, 4, 2}, {3, 6, 8} };
+        System.out.println(myNumbers[1][0]);
+
+        // Imprimindo uma matriz
+        for(int i = 0; i < myNumbers.length; i++) {
+            for(int j = 0; j < myNumbers[i].length; j++) {
+                System.out.println(myNumbers[i][j]);
+            }
+        }
+
+        // Comparando arrays
+        int[] arrayComparativo1 = new int[] {1,2,3};
+        int[] arrayComparativo2 = new int[] {1,2,3};
+        System.out.println(arrayComparativo1.equals(arrayComparativo2)); // False
 
     }
 }
