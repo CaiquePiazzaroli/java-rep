66cdad338887607513ad4668d8956ae45deb84b0
caique <caique405@gmail.com>
2026-05-27 20:17:18 -0300

    rev: Sting e java.time

--- Main.java
+++ Main.java
@@ -1,43 +1,29 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class Main {
-
-    static void exibirNome(String nome, int idade) {
-        System.out.println("Seu nome é: " + nome);
-        System.out.println("Voce tem " + idade + " anos");
-    }
-
-    static int calcularMaioridade(int idade) {
-
-        if (idade >= 18) {
-            return 0;
-        }
-
-        return 18 - idade;
-    }
-
-    static int calcularMaioridade(int idade, int parametroMaioridade) {
-
-        if (idade >= parametroMaioridade) {
-            return 0;
-        }
-
-        return parametroMaioridade - idade;
-    }
-
-
 
     public static void main(String[] args) {
 
-        exibirNome("Amanda", 24);
+        // Tipos primitivos
+		// Valores brutos que sao armazenados na memoria
+		// byte, short, int, long (inteiros: 20 30 40), float, double (decimais: 0.2, 15.7),
+		// char (caracter: 'c', 'z')
+		// boolean (verdadeiro ou falso: true ou false)
+		
+		Integer estoque = 15;
+		Double preco = 3999.99;
+		String nomeProduto = "Televisao Smart";
+        String data = "07/08/2023"; 
+        DateTimeFormatter formatador = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        LocalDate dataDeFabricacao = LocalDate.parse(data, formatador); // 2023-08-07
+        String linha = String.join(" - ", nomeProduto, estoque.toString(), preco.toString(), dataDeFabricacao.toString());
+        System.out.println(linha);
 
-        int idadePessoa = 15;
-        int anosParaMaioridade = calcularMaioridade(idadePessoa);
-        System.out.println("Uma pessoa de "
-                + idadePessoa +
-                " ainda tem que fazer mais " + anosParaMaioridade
-                + " para ser maiorde idade");
+        // nio 
 
-        int anosParaMaioridadeCustomizada = calcularMaioridade(idadePessoa, 24);
-        System.out.println(anosParaMaioridadeCustomizada);
 
-    }
+    }    
+  
+      
 }
