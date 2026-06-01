31b0b32cfbb614ce6bf65869ec27e08869dd3bbe
caique <caique405@gmail.com>
2026-06-01 13:15:36 -0300

    Rev: Java.time

--- Main.java
+++ Main.java
@@ -1,20 +1,55 @@
-import java.util.List;
-import java.util.Set;
-import java.util.TreeSet;
+import java.sql.Date;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.Period;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
+import java.util.Calendar;
+import java.util.Locale;
+import java.time.LocalDateTime;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        Set<String> nomes = new TreeSet<>();
-        nomes.add("caique");
-        nomes.add("joao");
-        nomes.add("maria");
-        nomes.add("ana");
+        //Data atual
+        LocalDate dataAtual = LocalDate.now();
+        System.out.println(dataAtual); // 01/06/2026
 
-        for(String e : nomes) {
-            System.out.println(e);
-        }
+
+        // Tempo atual
+        LocalTime tempoAtual = LocalTime.now();
+        System.out.println(tempoAtual); // 12:41:ss:yy
+
+        // Tempo e data atual
+        LocalDateTime horaEDataAtual = LocalDateTime.now();
+        System.out.println(horaEDataAtual); // yyyy-MM-ddTHH:mm:ss
+
+
+        // Formatando uma data e hora
+        DateTimeFormatter formatador = DateTimeFormatter.ofLocalizedDateTime(FormatStyle.SHORT).withLocale(Locale.CANADA);
+
+        String horaEDataAtualFormatada = horaEDataAtual.format(formatador);
+        System.out.println(horaEDataAtualFormatada);
+
+        // Retornando valores
+        System.out.println(dataAtual.getMonthValue()); // 06
+        System.out.println(dataAtual.getMonth()); // JUNE
+        System.out.println(dataAtual.withYear(2030)); // 2030-06-01
+
+        // Intente
+
+        Instant instante = Instant.ofEpochSecond(30L);
+        System.out.println(instante);
+
+        // Periodo
+        LocalDate hoje = LocalDate.now();
+        LocalDate aniversario = LocalDate.of(2026, 8, 03);
+
+        Period period = Period.between(hoje, aniversario);
+        System.out.println(period.getDays());
+        System.out.println(period.getMonths());
 
     }    
   
