8d9c21e05f5fa3993cb7136beaef7fe269dd025f
caique <caique405@gmail.com>
2026-06-04 11:43:27 -0300

    Rev: mapas

--- Main.java
+++ Main.java
@@ -1,62 +1,62 @@
 import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Deque;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.LinkedHashMap;
 import java.util.LinkedHashSet;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
 import java.util.Set;
 import java.util.Stack;
+import java.util.TreeMap;
 import java.util.TreeSet;
 import java.util.Vector;
+
+import java.util.Map;
 
 public class Main {
 
     public static void main(String[] args) {
         
-        // Conjuntos
+        // Mapas
 
-        // 1. Sem ordem 
-        Set<String> conjuntoSet = new HashSet<>();
-        conjuntoSet.add("caique");
-        conjuntoSet.add("joao");
-        conjuntoSet.add("maria");
-        conjuntoSet.add("amanda");
-        conjuntoSet.add("caique"); // Conjuntos nao permitem elementos duplicados
+        // 1. Sem garantia de ordem
+        Map<String, String> mapaSemOrdem = new HashMap<>();
+        mapaSemOrdem.put("caique", "mendes");
+        mapaSemOrdem.put("joao", "pereira");
+        mapaSemOrdem.put("maria", "do carmo");
 
-        for(String e: conjuntoSet) {
-            System.out.println(e);
+        for(String e: mapaSemOrdem.keySet()) {
+            System.out.println(e + " - " + mapaSemOrdem.get(e));
         }
 
-        System.out.println("------");
+        System.out.println("---------------");
 
-        // 2. Por ordem de inserção 
-        Set<String> conjuntoLinked = new LinkedHashSet<>();
-        conjuntoLinked.add("caique");
-        conjuntoLinked.add("joao");
-        conjuntoLinked.add("maria");
-        conjuntoLinked.add("amanda");
+        // 2. Sem garantia de ordem
+        Map<String, String> mapaOrdemInsercao = new LinkedHashMap<>();
+        mapaOrdemInsercao.put("caique", "mendes");
+        mapaOrdemInsercao.put("joao", "pereira");
+        mapaOrdemInsercao.put("maria", "do carmo");
 
-        for(String e: conjuntoLinked) {
-            System.out.println(e);
+        for(String e: mapaOrdemInsercao.keySet()) {
+            System.out.println(e + " - " + mapaOrdemInsercao.get(e));
         }
 
-        System.out.println("------");
+        System.out.println("---------------");
 
-        // 3. Por ordem natural (alfabetica)
-        Set<String> conjuntoAlfabetico = new TreeSet<>();
-        conjuntoAlfabetico.add("caique");
-        conjuntoAlfabetico.add("joao");
-        conjuntoAlfabetico.add("maria");
-        conjuntoAlfabetico.add("amanda");
+        // 3. Por ordem natural
+        Map<String, String> mapaOrdenadoNaturalmente = new TreeMap<>();
+        mapaOrdenadoNaturalmente.put("caique", "mendes");
+        mapaOrdenadoNaturalmente.put("joao", "pereira");
+        mapaOrdenadoNaturalmente.put("maria", "do carmo");
 
-        for(String e: conjuntoAlfabetico) {
-            System.out.println(e);
+        for(String e: mapaOrdenadoNaturalmente.keySet()) {
+            System.out.println(e + " - " + mapaOrdenadoNaturalmente.get(e));
         }
-
-        System.out.println("------");
-
+    
+    
         
     }    
   
