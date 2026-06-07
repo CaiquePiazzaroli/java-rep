5ac0fbdb70988942d035fe76d327dbbf23a8243b
caique <caique405@gmail.com>
2026-06-07 19:56:52 -0300

    Rev: Framwork Collections: Set

--- Main.java
+++ Main.java
@@ -1,46 +1,89 @@
-import java.util.Deque;
-import java.util.LinkedList;
-import java.util.PriorityQueue;
-import java.util.Queue;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Set;
+import java.util.TreeSet;
+
+class AlunoClasse implements Comparable<AlunoClasse>{
+
+    private String nome;
+    private Double nota;
+
+    public AlunoClasse(String nome, Double nota) {
+        this.nome = nome;
+        this.nota = nota;
+    }
+
+    
+
+    public String getNome() {
+        return nome;
+    }
+
+
+
+    public Double getNota() {
+        return nota;
+    }
+
+
+
+    @Override
+    public int compareTo(AlunoClasse o) {
+        if(this.getNota() > o.getNota()) {
+            return 1;
+        } else if (this.getNota() < o.getNota()) {
+            return -1;
+        } else {
+            return 0;
+        }
+    }
+
+    @Override
+    public int hashCode() {  
+        return this.nome.hashCode();
+    }
+    
+    @Override
+    public String toString() {
+        return this.nome.toString();
+    }
+}
+
+
+
 
 public class Main {
 
     public static void main(String[] args) {
 
-        // Fila
-        Queue<Integer> fila = new LinkedList<>();
-        fila.offer(Integer.valueOf(10));
-        fila.offer(Integer.valueOf(30));
-        System.out.println(fila.peek()); // Observa o primeiro
-        System.out.println(fila.poll()); // Observa e remove
-        System.out.println(fila);
+        // Set -> Conjuntos -> Não possuem elementos repetidos
 
-        Queue<Integer> filaPrioritaria = new PriorityQueue<>(); // Prioridade definida pelo compareTo do comparable
-        filaPrioritaria.offer(40);
-        filaPrioritaria.offer(30);
-        filaPrioritaria.offer(754);
-        System.out.println(filaPrioritaria);
+        // HashSet -> Sem ordem
+        Set<String> nomes = new HashSet<>();
+        nomes.add("Caique Mendes");
+        nomes.add("Joao Aparecido");
+        nomes.add("Cleide Soares");
+        nomes.remove("Joao Aparecido");
+        nomes.contains("Caique Mendes");
+        nomes.size();
+        nomes.clear();
 
-        // Pilha
-        // Criando uma pilha de Strings usando LinkedList (LIFO)
-        Deque<String> livros = new LinkedList<>();
+        // TreeSet -> Ordem natural ou pela implementação do compareTo + hashCode
+        Set<AlunoClasse> turma = new TreeSet<>();
+        turma.add(new AlunoClasse("Caique Mendes", 9.2));
+        turma.add(new AlunoClasse("Joao Aparecido", 6.0));
+        turma.add(new AlunoClasse("Clotilde Almeida", 7.2));
+        turma.add(new AlunoClasse("Joao Guelrras", 4.45));
+        System.out.println(turma); // Ordem alfabetica
 
-        // Empilhando (Push)
-        livros.push("Senhor dos aneis");
-        livros.push("A guerra dos tronos");
-        livros.push("Duna");
-        livros.push("Hobbit");
-        //                                                   (Topo)                               (Fundo)
-        System.out.println("Pilha inicial: " + livros); // [Hobbit, Duna, A guerra dos tronos, Senhor dos aneis]
 
-        // Espiando o topo (Peek)
-        System.out.println("Topo da pilha: " + livros.peek()); // Saída: Hobbit
-
-        // Desempilhando (Pop)
-        System.out.println("Removido: " + livros.pop()); // Desempilha Hobbit
-        System.out.println("Novo topo: " + livros.peek()); // Duna
-
-        System.out.println("Pilha Final: " + livros); // [Duna, A guerra dos tronos, Senhor dos aneis]
+        // LinkedHashSet -> Por ordem de chegada
+        Set<AlunoClasse> turmaOrdemDeChegada = new LinkedHashSet<>();
+        turmaOrdemDeChegada.add(new AlunoClasse("Caique Mendes", 9.2));
+        turmaOrdemDeChegada.add(new AlunoClasse("Joao Aparecido", 6.0));
+        turmaOrdemDeChegada.add(new AlunoClasse("Clotilde Almeida", 7.2));
+        turmaOrdemDeChegada.add(new AlunoClasse("Joao Guelrras", 4.45));
+        System.out.println(turmaOrdemDeChegada);
     }
 
 }
