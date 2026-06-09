7ba82ae8618808f23cea95802a08bbd2fb29d416
caique <caique405@gmail.com>
2026-06-09 13:40:00 -0300

    rev: Maps

--- Main.java
+++ Main.java
@@ -1,6 +1,12 @@
+import java.util.Collection;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.LinkedHashMap;
 import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Map;
 import java.util.Set;
+import java.util.TreeMap;
 import java.util.TreeSet;
 
 class AlunoClasse implements Comparable<AlunoClasse>{
@@ -56,34 +62,47 @@ public class Main {
 
     public static void main(String[] args) {
 
-        // Set -> Conjuntos -> Não possuem elementos repetidos
+        // Map -> Chave e Valor
+        // HashMap -> Sem ordem
+        Map<String, Integer> veiculos = new HashMap<>();
+    
+        // Adiciona
+        veiculos.put("ford", 2006);
+        veiculos.put("corsa", 2001);
+        veiculos.put("hyundai", 2010);
+        
+        // Resgata
+        veiculos.get("corsa");
+        
+        // Remove
+        veiculos.remove("ford");
 
-        // HashSet -> Sem ordem
-        Set<String> nomes = new HashSet<>();
-        nomes.add("Caique Mendes");
-        nomes.add("Joao Aparecido");
-        nomes.add("Cleide Soares");
-        nomes.remove("Joao Aparecido");
-        nomes.contains("Caique Mendes");
-        nomes.size();
-        nomes.clear();
+        // Verifica se existe a chave
+        veiculos.containsKey("hyundai");
 
-        // TreeSet -> Ordem natural ou pela implementação do compareTo + hashCode
-        Set<AlunoClasse> turma = new TreeSet<>();
-        turma.add(new AlunoClasse("Caique Mendes", 9.2));
-        turma.add(new AlunoClasse("Joao Aparecido", 6.0));
-        turma.add(new AlunoClasse("Clotilde Almeida", 7.2));
-        turma.add(new AlunoClasse("Joao Guelrras", 4.45));
-        System.out.println(turma); // Ordem alfabetica
+        // Retorna as chaves
+        veiculos.keySet();
 
+        // Retorna os valores
+        veiculos.values();
 
-        // LinkedHashSet -> Por ordem de chegada
-        Set<AlunoClasse> turmaOrdemDeChegada = new LinkedHashSet<>();
-        turmaOrdemDeChegada.add(new AlunoClasse("Caique Mendes", 9.2));
-        turmaOrdemDeChegada.add(new AlunoClasse("Joao Aparecido", 6.0));
-        turmaOrdemDeChegada.add(new AlunoClasse("Clotilde Almeida", 7.2));
-        turmaOrdemDeChegada.add(new AlunoClasse("Joao Guelrras", 4.45));
-        System.out.println(turmaOrdemDeChegada);
+        System.out.println(veiculos);
+
+        // TreeMap -> Ordenado pelas chaves
+        Map<String, Integer> veiculosOrdenados = new TreeMap<>();
+        veiculosOrdenados.put("ford", 2006); // 2
+        veiculosOrdenados.put("corsa", 2001); // 1 
+        veiculosOrdenados.put("hyundai", 2010); // 3
+
+        System.out.println(veiculosOrdenados);
+
+        // LinkedHashMap -> Por ordem de inserção
+        Map<String, Integer> ordemInsercao = new LinkedHashMap<>();
+        ordemInsercao.put("ford", 2006);
+        ordemInsercao.put("corsa", 2001); 
+        ordemInsercao.put("hyundai", 2010); 
+
+        System.out.println(ordemInsercao);
     }
 
 }
