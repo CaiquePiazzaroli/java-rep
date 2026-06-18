42425f909c432f064b769902c6c22507d8c452cd
caique <caique405@gmail.com>
2026-06-18 14:24:08 -0300

    rev: algoritmos em java usando Collections

--- EstruturaDeDados/Aula20EmbaralhandoColecoes.java
+++ EstruturaDeDados/Aula20EmbaralhandoColecoes.java
@@ -6,13 +6,13 @@ import java.util.Collections;
 public class Aula20EmbaralhandoColecoes {
     public static void main(String[] args) {
         ArrayList<String> cards = new ArrayList<>();
-    cards.add("Ace");
-    cards.add("King");
-    cards.add("Queen");
-    cards.add("Jack");  
+        cards.add("Ace");
+        cards.add("King");
+        cards.add("Queen");
+        cards.add("Jack");  
 
-    System.out.println(cards);
-    Collections.shuffle(cards);
-    System.out.println(cards);
+        System.out.println(cards);
+        Collections.shuffle(cards);
+        System.out.println(cards);
     }
 }
--- Main.java
+++ Main.java
@@ -1,65 +1,6 @@
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Collections;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.LinkedHashMap;
-import java.util.LinkedHashSet;
 import java.util.List;
-import java.util.Map;
-import java.util.Set;
-import java.util.TreeMap;
-import java.util.TreeSet;
-
-class AlunoClasse implements Comparable<AlunoClasse>{
-
-    private String nome;
-    private Double nota;
-
-    public AlunoClasse(String nome, Double nota) {
-        this.nome = nome;
-        this.nota = nota;
-    }
-
-    
-
-    public String getNome() {
-        return nome;
-    }
-
-
-
-    public Double getNota() {
-        return nota;
-    }
-
-
-
-    @Override
-    public int compareTo(AlunoClasse o) {
-        if(this.getNota() > o.getNota()) {
-            return 1;
-        } else if (this.getNota() < o.getNota()) {
-            return -1;
-        } else {
-            return 0;
-        }
-    }
-
-    @Override
-    public int hashCode() {  
-        return this.nome.hashCode();
-    }
-    
-    @Override
-    public String toString() {
-        return this.nome.toString();
-    }
-}
-
-
-
 
 public class Main {
 
@@ -69,39 +10,39 @@ public class Main {
         names.add("Chris Redfield");
         names.add("Harry Potter");
         names.add("James Sunderland");
-        names.add("Clarie Redfield");
-        names.add("Harry Potter");
-        names.add("James Sunderland");
-        names.add("Harry Potter");
-        names.add("James Sunderland");
+        names.add("Gerald de Rívia");
+        names.add("Marika");
 
-        ArrayList<Integer> numbers = new ArrayList<>();
-        numbers.add(5);
-        numbers.add(1);
-        numbers.add(7);
-        numbers.add(3);
-        numbers.add(9);
+        System.out.println("Ordem inicial: " + names);
 
-        System.out.println("Max: " + Collections.max(numbers));
-        System.out.println("Min: " + Collections.min(numbers));
+        // sort - Ordenacao por ordem natural (ou Comparable)
+        Collections.sort(names);
+        System.out.println("Ordenado alfabeticamente: " + names);
+        
+        // reverse
+        Collections.reverse(names);
+        System.out.println("Realizando inversao: " + names);
+        
+        // shuffle
+        Collections.shuffle(names);
+        System.out.println("Embaralhando os elementos: " + names);
+    
+        // addAll
+        Collections.addAll(names, "Clarie Redfield", "Leon Scott Kennedy", "Rhaenyra Targaryen"); 
+        System.out.println(names);
 
-        Collections.swap(names, 0, 7); // Troca elementos
-        Collections.sort(names); // Ordena por ordem natural ou pela implementação do compareTo
-        Collections.binarySearch(names, "James Sunderland"); // faz uma busca do elemento por busca binaria
-        Collections.shuffle(names); // Bagunça a lista
-        Collections.frequency(names, "Harry Potter"); // Retorna quantas vezes um determinado elemento aparece
-
-        // Iteracoes
-        for(String c: names) {
-            System.out.println(c);
+        // binarySearch
+        int index = Collections.binarySearch(names, "Rhaenyra Targaryen");
+        if(index > 0) {
+            System.out.println("Encontrado " + names.get(index) + " com índice " + index);
+        } else {
+            System.out.println("Elemento nao encontrado");
         }
 
-        Iterator<String> it = names.iterator();
-        while (it.hasNext()) {
-            System.out.println(it.next());
-        }
-
-
+        // Swap - Trocando elementos de lugar
+        Collections.swap(names, 6, 7);
+        System.out.println(names);
+        
     }
 
 }
